df2f6df11f28c771004f50bc48452eca901843a4
Tiny Taimi <OpNop@users.noreply.github.com>
2022-02-24 22:00:15 -0700

    Created ServerVersion Packet

---
+++ Lib/Packets/Server/ServerVersion.cs
@@ -0,0 +1,33 @@
+﻿using TinySocket;
+
+namespace TACSLib.Packets.Server
+{
+    public class ServerVersion : IPacket
+    {
+        private readonly PacketType _packetType = PacketType.S_SERVER_VER;
+
+        public string ServerRSA { get; private set; }
+
+        public ServerVersion(string serverRSA)
+        {
+            ServerRSA = serverRSA;
+        }
+
+        public ServerVersion(Unpacker p)
+        {
+            ServerRSA = p.GetASCIIString();
+        }
+
+        public byte[] Pack()
+        {
+            var p = new Packer((byte)_packetType);
+            p.AddString(ServerRSA);
+            return p.ToArray();
+        }
+
+        public PacketType ID
+        {
+            get { return _packetType; }
+        }
+    }
+}
