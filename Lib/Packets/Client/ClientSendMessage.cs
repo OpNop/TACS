3f4528df995979f6ff410cf2371b1acb3b325a72
Tiny Taimi <OpNop@users.noreply.github.com>
2022-02-24 21:59:34 -0700

    Renamed SendMessage to ClientSendMessage

---
+++ Lib/Packets/Client/ClientSendMessage.cs
@@ -0,0 +1,32 @@
+﻿using TinySocket;
+
+namespace TACSLib.Packets.Client
+{
+    public class ClientSendMessage : IPacket
+    {
+        private readonly PacketType _packetType = PacketType.C_SEND_MESSAGE;
+        public string Message { get; private set; }
+
+        public ClientSendMessage(string message)
+        {
+            Message = message;
+        }
+
+        public ClientSendMessage(Unpacker p)
+        {
+            Message = p.GetString();
+        }
+
+        public byte[] Pack()
+        {
+            var p = new Packer((byte)_packetType);
+            p.AddString(Message);
+            return p.ToArray();
+        }
+
+        public PacketType ID
+        {
+            get { return _packetType; }
+        }
+    }
+}
