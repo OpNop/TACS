bb71f72278c1e784b20599ee350ed8ee679b6ab0
Tiny Taimi <OpNop@users.noreply.github.com>
2022-03-04 19:45:17 -0700

    Start of v2 client UI
    thinking should make some generic styles for
    - small buttons
    - large buttons
    - text box
    - large text box
    - check box

---
+++ Client/Views/APIWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TACS_Client.Views
+{
+    /// <summary>
+    /// Interaction logic for APIWindow.xaml
+    /// </summary>
+    public partial class APIWindow : Window
+    {
+        public APIWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CreateAPI_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Process.Start(new ProcessStartInfo()
+            {
+                UseShellExecute = true,
+                FileName = "https://account.arena.net/applications",
+            });
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show($"API Key: {ApiKeyInput.Text}");
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+                DragMove();
+        }
+
+        private void Image_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Environment.Exit(0);
+        }
+    }
+}
